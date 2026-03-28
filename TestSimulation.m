a71733ab46b8be889c693669b53934a25e3c16ce
Adam Yarrow <adam.yarrow@colorado.edu>
2026-03-28 15:25:10 -0600

    Adding plotting

--- TestSimulation.m
+++ TestSimulation.m
@@ -1,15 +1,26 @@
-
-debrisIC = createCircularOrbitIC(400,0);
+%% Generate Data
+debrisIC = createCircularOrbitIC(10000,0);
 endTime = 1000;
 
-simData = Simulation([0,pi/4], [0], debrisIC, endTime);
+% Full Case
+thetaRx = linspace(0,2*pi,12+1); % every 30 deg
+thetaGPS = linspace(0,2*pi,31); % Assuming 30 GPS satellites visible
 
+% Testing case
+thetaRx = [0,pi/2];
+thetaGPS = [0,pi/4];
+
+% Generate Sim Data
+fRegen = false;
+if fRegen || ~exist('simData','var')
+    simData = Simulation(thetaGPS,thetaRx, debrisIC, endTime);
+end
+
+
+%% Plotting
+skipFrames = 9;
+fPlotTxRx = true;
+plotSimulation(simData,skipFrames,fPlotTxRx);
 
 % TODO - make a plot that shows lines eminating between Rx and Tx etc when
 % they are in view of one another
-
-function plotSim(simData)
-    
-    
-    
-end
