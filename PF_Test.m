4b76223031ad34d26365c8bdf8f6beed72d44260
Adam Yarrow <adam.yarrow@colorado.edu>
2026-04-23 08:15:23 -0600

    Updates to KS testing.

--- PF_Test.m
+++ PF_Test.m
@@ -1,24 +1,40 @@
+%% Vanilla Sim Data Generation 
+% rng(0);
 params = ModelParams();
-params.endTime = 500;
-% params.est.pf.measNoiseCov = params.est.pf.measNoiseCov / 1000;
-if ~exist('simDataTest','var')
-    % load('SavedData\simData_halfOrbit.mat')    
+params.endTime = 50;
+if ~exist('simDataTest','var') 
     simDataTest = GenerateSimData(false, params);
 end
-% save('SavedData\simData_halfOrbit.mat','simData');
-
-rng(0);
 
 [mu0, P0] = makePriorDistribution(params);
 Np = 1000;
 
-% profile on 
+%% Warm start with NLS
+rng(0);
+[x0Est_NLS, P_NLS, ~] = NLS_WarmStart();
+
+mu0(2) = x0Est_NLS(2);
+mu0(4) = x0Est_NLS(4);
+
+% Update only pure velocity terms in covariance matrix
+P0(2,2) = P_NLS(2,2);
+P0(2,4) = P_NLS(2,4);
+P0(4,4) = P_NLS(4,4);
+P0(4,2) = P_NLS(4,2);
+
+%% Run Filter
 type = params.est.pf.type;
 nWorkers = 6; % 0 --> serial, > 1 --> Parallel pools
 pfResultsTest = Run_PF(type, Np, simDataTest, P0, mu0, nWorkers, params);
-% profile viewer;
 
 plotPF_Results(simDataTest,pfResultsTest,params);
 
 %% KS Test
-% [dt_history, dtMean, testStatistic] = PF_KS_Test(simDataTest,pfResultsTest,params,nWorkers);
+[dt_history, dtMean, testStatistic,varDmean_t,Dt_mu] =...
+        PF_KS_Test(simDataTest, pfResultsTest, params,nWorkers);
+
+plotPF_KS_Test(pfResultsTest, varDmean_t, testStatistic);
+
+
+
+
---
+++ Plotting/plotPF_KS_Test.m
@@ -0,0 +1,28 @@
+function plotPF_KS_Test(pfResults, DmeanVariance, qt)
+figure('Name','PF KS Test Results');
+subplot(1,2,1); 
+hold on;
+h = plot(pfResults.t, squeeze(qt(1,:,:))','k*');
+set(h(2:end),'HandleVisibility','off');
+plot(pfResults.t, 2*DmeanVariance.^0.5,'r--');
+plot(pfResults.t, -2*DmeanVariance.^0.5,'r--');
+legend('Test Statistic', '2-sigma Bound');
+title('Doppler Shift Measurements');
+grid on;
+xlabel('Time (s)');
+ylabel('KS Test Statistic');
+
+subplot(1,2,2); 
+hold on;
+h2 =plot(pfResults.t, squeeze(qt(2,:,:))','k*');
+set(h2(2:end),'HandleVisibility','off');
+plot(pfResults.t, 2*DmeanVariance.^0.5,'r--');
+plot(pfResults.t, -2*DmeanVariance.^0.5,'r--');
+legend('Test Statistic', '2-sigma Bound');
+title('Time of Flight Measurements');
+grid on;
+xlabel('Time (s)');
+ylabel('KS Test Statistic');
+
+sgtitle('KS Test Statistic Vs Time')
+end
