b3921bcc706d16544c2ac71784a7ccb21df7e40a
Adam Yarrow <adam.yarrow@colorado.edu>
2026-04-26 17:48:34 -0600

    Saving figures and testing from KS test and NEES and warm/cost start.

--- PF_Test.m
+++ PF_Test.m
@@ -1,5 +1,5 @@
 %% Vanilla Sim Data Generation 
-% rng(0);
+rng(0);
 params = ModelParams();
 params.endTime = 50;
 if ~exist('simDataTest','var') 
@@ -10,31 +10,38 @@ end
 Np = 1000;
 
 %% Warm start with NLS
-rng(0);
-[x0Est_NLS, P_NLS, ~] = NLS_WarmStart();
+fWarmStart = true;
+if fWarmStart
+    rng(0);
 
-mu0(2) = x0Est_NLS(2);
-mu0(4) = x0Est_NLS(4);
-
-% Update only pure velocity terms in covariance matrix
-P0(2,2) = P_NLS(2,2);
-P0(2,4) = P_NLS(2,4);
-P0(4,4) = P_NLS(4,4);
-P0(4,2) = P_NLS(4,2);
+    [x0Est_NLS, P_NLS, ~] = NLS_WarmStart();
+    
+    mu0(2) = x0Est_NLS(2);
+    mu0(4) = x0Est_NLS(4);
+    
+    % Update only pure velocity terms in covariance matrix
+    P0(2,2) = P_NLS(2,2);
+    P0(2,4) = P_NLS(2,4);
+    P0(4,4) = P_NLS(4,4);
+    P0(4,2) = P_NLS(4,2);
+end
 
 %% Run Filter
 type = params.est.pf.type;
 nWorkers = 6; % 0 --> serial, > 1 --> Parallel pools
+
 pfResultsTest = Run_PF(type, Np, simDataTest, P0, mu0, nWorkers, params);
 
 plotPF_Results(simDataTest,pfResultsTest,params);
 
 %% KS Test
-[dt_history, dtMean, testStatistic,varDmean_t,Dt_mu] =...
-        PF_KS_Test(simDataTest, pfResultsTest, params,nWorkers);
-
-plotPF_KS_Test(pfResultsTest, varDmean_t, testStatistic);
-
+fRunKS = false;
+if fRunKS
+    [dt_history, dtMean, testStatistic,varDmean_t,Dt_mu] =...
+            PF_KS_Test(simDataTest, pfResultsTest, params,nWorkers);
+    
+    plotPF_KS_Test(pfResultsTest, varDmean_t, testStatistic);
+end
 
 
 
--- Plotting/plotPF_KS_Test.m
+++ Plotting/plotPF_KS_Test.m
@@ -2,7 +2,7 @@ function plotPF_KS_Test(pfResults, DmeanVariance, qt)
 figure('Name','PF KS Test Results');
 subplot(1,2,1); 
 hold on;
-h = plot(pfResults.t, squeeze(qt(1,:,:))','k*','LineWidth',1.5);
+h = plot(pfResults.t, squeeze(qt(1,:,:))','ko');
 set(h(2:end),'HandleVisibility','off');
 plot(pfResults.t, 2*DmeanVariance.^0.5,'r--','LineWidth',1.5);
 plot(pfResults.t, -2*DmeanVariance.^0.5,'r--','LineWidth',1.5);
@@ -17,7 +17,7 @@ ax.FontSize = 12;
 
 subplot(1,2,2); 
 hold on;
-h2 =plot(pfResults.t, squeeze(qt(2,:,:))','k*','LineWidth',1.5);
+h2 =plot(pfResults.t, squeeze(qt(2,:,:)),'ko');
 set(h2(2:end),'HandleVisibility','off');
 plot(pfResults.t, 2*DmeanVariance.^0.5,'r--','LineWidth',1.5);
 plot(pfResults.t, -2*DmeanVariance.^0.5,'r--','LineWidth',1.5);
